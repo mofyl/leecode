c06a4fcd289ff670394bd9997568244c18737940
lirui5 <894495408@qq.com>
2020-06-27 13:06:54 +0800

    fix 	modified:   .gitignore 	modified:   getPermutation_60_test.go 	new file:   search_33_test.go

--- getPermutation_60_test.go
+++ getPermutation_60_test.go
@@ -1,16 +1,15 @@
 package main
 
-import "testing"
-
-func TestGetPermutation(t *testing.T) {
-
-}
-
-func getPermutation(n int, k int) string {
-	nums := make([]int, n)
-
-	for i := 0; i < n; i++ {
-		nums[i] = i + 1
-	}
-
-}
+//
+//func TestGetPermutation(t *testing.T) {
+//
+//}
+//
+//func getPermutation(n int, k int) string {
+//	nums := make([]int, n)
+//
+//	for i := 0; i < n; i++ {
+//		nums[i] = i + 1
+//	}
+//
+//}
