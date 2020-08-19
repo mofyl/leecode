27cc9902810b5c0ce49f36b3dc4ddc5322c99466
mō <mo@modeMac-mini.local>
2020-08-19 17:59:35 +0800

    fix 	new file:   lengthOfLongestSubstring_3_test.go 	modified:   ../V1/codec_297_test.go

---
+++ V2/lengthOfLongestSubstring_3_test.go
@@ -0,0 +1,35 @@
+package V2
+
+func lengthOfLongestSubstring(s string) int {
+
+	resStr := ""
+
+	curLen := 0
+	maxLen := 0
+
+
+	for i := 0 ; i < len(s) ; i++{
+
+		str := string(s[i])
+		for strings.Contains(resStr,str ){
+
+			if curLen > 0 {
+			resStr = resStr[1:curLen]
+			curLen--
+
+			}
+		}
+
+		resStr+= str
+		curLen++
+
+		if  maxLen < curLen {
+			maxLen = curLen
+		}
+
+
+	}
+
+	return maxLen
+
+}
