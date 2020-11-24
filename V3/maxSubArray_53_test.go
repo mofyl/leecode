af28786c00dc6b6edf1da82ff25709bb04391881
lirui <894495408@qq.com>
2020-11-24 17:37:21 +0800

    fix 	new file:   maxSubArray_53_test.go 	new file:   rotate_48_test.go

---
+++ V3/maxSubArray_53_test.go
@@ -0,0 +1,33 @@
+package V3
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMaxSubArray(t *testing.T) {
+
+	nums := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
+	res := maxSubArray(nums)
+
+	fmt.Println(res)
+}
+
+func maxSubArray(nums []int) int {
+
+	sum := 0
+	ans := 0
+	for i := 0; i < len(nums); i++ {
+		if sum > 0 {
+			sum += nums[i]
+		} else {
+			sum = nums[i]
+		}
+
+		if sum > ans {
+			ans = sum
+		}
+
+	}
+	return ans
+}
