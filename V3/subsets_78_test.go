e481ce0535f7e283c90a92db804ddc92d1be0823
lirui <894495408@qq.com>
2020-12-09 17:50:14 +0800

    fix 	new file:   combinationSum_39_V2_test.go 	new file:   getPermutation_60_test.go 	new file:   permuteUnique_47_test.go 	new file:   permute_46._test.go 	new file:   solveNQueens_51_test.go 	new file:   solveSudoku_37_test.go 	new file:   subsetsWithDup_90_test.go 	new file:   subsets_78_test.go

---
+++ V3/subsets_78_test.go
@@ -0,0 +1,46 @@
+package V3
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSubTest(t *testing.T) {
+
+	nums := []int{1, 2, 3}
+
+	res := subsets(nums)
+
+	fmt.Println(res)
+}
+
+func subsets(nums []int) [][]int {
+	c := make([][]int, 0, len(nums))
+	if len(nums) < 1 {
+		return c
+	}
+	c = append(c, []int{})
+	subsetsLoop(nums, 0, &[]int{}, &c)
+	return c
+}
+
+func subsetsLoop(nums []int, start int, cur *[]int, res *[][]int) {
+
+	if start > len(nums) {
+		return
+	}
+
+	if len(*cur) > 0 {
+		tmp := make([]int, len(*cur))
+		copy(tmp, *cur)
+		*res = append(*res, tmp)
+	}
+
+	for i := start; i < len(nums); i++ {
+
+		*cur = append(*cur, nums[i])
+		subsetsLoop(nums, i+1, cur, res)
+		*cur = (*cur)[:len(*cur)-1]
+	}
+
+}
